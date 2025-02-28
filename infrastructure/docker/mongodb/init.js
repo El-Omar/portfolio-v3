1ba0114c2016b6368db982c4bf89abfb820c8b92
El-Omar <elomar.sami@gmail.com>
2025-02-28 12:00:50 +0100

    Update MongoDB initialization script and Docker configurations

--- infrastructure/docker/mongodb/init.js
+++ infrastructure/docker/mongodb/init.js
@@ -1,10 +1,10 @@
 db = db.getSiblingDB("admin");
-db.auth(
-  process.env.MONGO_INITDB_ROOT_USERNAME,
-  process.env.MONGO_INITDB_ROOT_PASSWORD
-);
+// db.auth(
+//   process.env.MONGO_INITDB_ROOT_USERNAME,
+//   process.env.MONGO_INITDB_ROOT_PASSWORD
+// );
 
-db = db.getSiblingDB(process.env.MONGODB_DATABASE);
+db = db.getSiblingDB(process.env.MONGODB_DATABASE || "portfolio");
 
 // Creating the collections that I have
 db.createCollection("projects");
@@ -14,3 +14,5 @@ db.createCollection("blogs");
 db.projects.createIndex({ slug: 1 }, { unique: true });
 db.projects.createIndex({ featured: 1, order: 1 });
 db.blogs.createIndex({ slug: 1 }, { unique: true });
+
+print("MongoDB initialization completed successfully");
