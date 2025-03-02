b152e0c7d7b15943455f531f18420f931a95dcd0
El-Omar <elomar.sami@gmail.com>
2025-03-02 13:02:06 +0100

    Add S3 media bucket hostname to Next.js image configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -18,6 +18,10 @@ const nextConfig: NextConfig = {
         hostname: `${env.AWS_S3_BUCKET}.s3.${env.AWS_REGION}.amazonaws.com`,
         protocol: "https",
       },
+      {
+        hostname: "elomar-portfolio-v3-media.s3.eu-central-1.amazonaws.com",
+        protocol: "https",
+      },
     ],
   },
   experimental: {
